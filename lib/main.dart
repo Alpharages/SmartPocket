1e9ae35935c1751fba0963e991e47c6933752b43
alpharages <hasnain.abbas@alpharages.com>
2025-05-07 16:09:19 +0500

    Login,Signup,Session Management,Firebase Integration.

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,9 @@ class MyApp extends StatelessWidget {
         '/login': (context) => const LoginScreen(),
         '/signup': (context) => const SignupScreen(),
         '/home': (context) => const MainNavigation(),
+
+
+
         // Add more routes as needed
       },
     );
